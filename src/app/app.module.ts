843d864f25521526043a354d059c6429fdeea818
Jorge L Avila G <jorgelavilag@Jorge-L-Avila-Gs-MacBook-Pro.local>
2018-09-06 17:09:17 -0500

    making the doc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 import { AppComponent } from './app.component';
+import { ChatComponent } from './chat/chat.component';
+import { MessageComponent } from './message/message.component';
+import { InputComponent } from './input/input.component';
+
+const config = {
+  apiKey: "AIzaSyCw4ky67yR--UjZuXl9w2ICtx9xiquxgBk",
+  authDomain: "ng-colombiafirebase.firebaseapp.com",
+  databaseURL: "https://ng-colombiafirebase.firebaseio.com",
+  projectId: "ng-colombiafirebase",
+  storageBucket: "ng-colombiafirebase.appspot.com",
+  messagingSenderId: "760871696679"
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent,
+    MessageComponent,
+    InputComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(config),
+    AngularFireDatabaseModule,
+    AngularFirestoreModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
